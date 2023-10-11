48cc7e5ff6f5cc7859f0017572a0ad1730a2e3fa
rushi-smb <rushi@smbios.co.in>
2023-10-11 21:35:08 +0530

    Add Pages(Views) and Models in code using repository help

--- DevsPark/MVVM/Models/Place.cs
+++ DevsPark/MVVM/Models/Place.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace DevsPark.MVVM.Models
 {
-    internal class Place
+    public class Place
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
     }
 }
